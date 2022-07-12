09974cba6b5770a19574e9d5662bbe08dbf328fe
Nick Aralin <nikkofann@gmail.com>
2022-07-12 12:31:28 -0400

    set up typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
@@ -20,10 +20,11 @@ module.exports = {
         use: ["style-loader", "css-loader", "less-loader", "postcss-loader"],
       },
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js)$/,
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      { test: /\.ts|tsx$/, exclude: /node_modules/, use: "ts-loader" },
     ],
   },
   resolve: {
